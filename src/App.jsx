2dd44034eea39d5925af876f3430f4bb53d82556
bora <nhatbaoxxd@gmail.com>
2023-11-20 15:48:45 +0700

    handle login on the client

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import axios from 'axios'
+import { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
 
 function App() {
+  useEffect(() => {
+    const controller = new AbortController()
+
+    axios
+      .get('/users/me', {
+        headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` },
+        baseURL: import.meta.env.VITE_API_URL,
+        signal: controller.signal,
+      })
+      .then((res) => {
+        const profile = res.data.result
+        localStorage.setItem('profile', JSON.stringify(profile))
+      })
+  }, [])
+
   return (
     <>
       <RouterProvider router={router} />
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   const isAuthenticated = Boolean(localStorage.getItem('access_token'))
+  const profile = JSON.parse(localStorage.getItem('profile')) || {}
+
   const logout = () => {
     localStorage.removeItem('access_token')
     localStorage.removeItem('refresh_token')
@@ -13,7 +15,7 @@ export default function Home() {
     <>
       {isAuthenticated ? (
         <div className="flex flex-col gap-5">
-          <span>Hello my friend, you are logged in.</span>
+          <span>Hello my {profile.name}, you are logged in.</span>
           <Link
             to={PATH.watch}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-36"
